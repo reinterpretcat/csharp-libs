f0fd21bc6ca8bf5f88ee9d331c8b2814d6ec9730
Ilya Builuk <ilya.builuk@gmail.com>
2016-06-11 00:37:31 +0200

    utyrx: make stubs visible

--- utyrx/UtyRx/System/Observer.cs
+++ utyrx/UtyRx/System/Observer.cs
@@ -489,7 +489,7 @@ namespace UtyRx
         }
     }
 
-    internal static class Stubs
+    public static class Stubs
     {
         public static readonly Action Nop = () => { };
         public static readonly Action<Exception> Throw = ex => { throw ex; };
@@ -501,21 +501,21 @@ namespace UtyRx
         }
     }
 
-    internal static class Stubs<T>
+    public static class Stubs<T>
     {
         public static readonly Action<T> Ignore = (T t) => { };
         public static readonly Func<T, T> Identity = (T t) => t;
         public static readonly Action<Exception, T> Throw = (ex, _) => { throw ex; };
     }
 
-    internal static class Stubs<T1, T2>
+    public static class Stubs<T1, T2>
     {
         public static readonly Action<T1, T2> Ignore = (x, y) => { };
         public static readonly Action<Exception, T1, T2> Throw = (ex, _, __) => { throw ex; };
     }
 
 
-    internal static class Stubs<T1, T2, T3>
+    public static class Stubs<T1, T2, T3>
     {
         public static readonly Action<T1, T2, T3> Ignore = (x, y, z) => { };
         public static readonly Action<Exception, T1, T2, T3> Throw = (ex, _, __, ___) => { throw ex; };
