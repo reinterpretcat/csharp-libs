5e4703bac1bbd8f88d41018d6a145441f0b0adc2
Ilya Builuk <ilya.builuk@gmail.com>
2016-06-10 23:44:09 +0200

    utyrx: add cancelation token

---
+++ utyrx/UtyRx/System/CancellationToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UtyRx
+{
+    public class CancellationToken
+    {
+        private readonly ICancelable _source;
+
+        public static CancellationToken Empty = new CancellationToken(new BooleanDisposable());
+
+        public CancellationToken(ICancelable source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            this._source = source;
+        }
+
+        public bool IsCancellationRequested
+        {
+            get
+            {
+                return _source.IsDisposed;
+            }
+        }
+
+        public void ThrowIfCancellationRequested()
+        {
+            if (IsCancellationRequested)
+            {
+                throw new OperationCanceledException();
+            }
+        }
+    }
+}
